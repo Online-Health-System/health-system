347a30017e3e387603db51f0d565d79ba6ef194c
1dina <dinafadel69@gmail.com>
2025-12-30 02:43:16 +0200

    feat(doctor): implement initial structure for medical notes modal

--- modules/doctor.js
+++ modules/doctor.js
@@ -1,1 +1,19 @@
-//Intial structure for doctor module
+// import { checkAccess } from "./auth/auth"; 
+// checkAccess(['admin', 'doctor']);
+console.log("Doctor module loaded");
+const modal = document.getElementById('medicalNotesModal');
+const notesList = document.getElementById('notesList');
+const newNote = document.getElementById('newNote');
+const addNoteBtn = document.getElementById('addNoteBtn');
+const closeBtn = document.getElementById('closeNotesBtn');
+let currentPatientId = null;
+document.querySelectorAll('.show_medical').forEach(btn => {
+    btn.onclick = () => {
+        const row = btn.closest('tr');
+        currentPatientId = row.dataset.currentPatientId;
+        //renderNotes();
+        modal.style.display = 'flex';
+    }
+});
+
+closeBtn.onclick = () => modal.style.display = 'none';
